88db627627b0bbcdcb15dda77a27f09b222b0a5e
Iverson <ivcotad@gmail.com>
2024-05-11 09:02:17 -0700

    dockerize the base applications
    add db
    
    dockerize backend
    
    update backend output
    
    add to dev container

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { name: string } {
+    return {
+      name: "Opus 3.5"
+
+    }
   }
 }
